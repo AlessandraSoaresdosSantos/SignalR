19e7a29516aa4017b3f7dfb527a421f842d7eb32
AlessandraSoaresdosSantos <alessandradesenvolvedora@gmail.com>
2019-07-14 01:51:19 -0300

    .

---
+++ SignalR/SignalR.Controller/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Controller.Hubs;
+
+namespace SignalR.Controller.Controllers
+{
+    public class MessageController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IHubContext<MessageHub> _hubContext;
+
+        public MessageController(IHubContext<MessageHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet("/message")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/message")]
+        public async Task<IActionResult> Post([FromForm] string message)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SignalR/SignalR.Controller/Startup.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalR.Controller.Hubs;
+
+namespace SignalR.Controller
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+            });
+
+
+            services.AddRazorPages();
+            services.AddMvc();
+
+            services.AddSignalR();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+
+           // app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseMvc();
+
+            app.UseSignalR(config =>
+            {
+                config.MapHub<MessageHub>("/messages");
+
+            });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapRazorPages();
+            //});
+        }
+    }
+}
